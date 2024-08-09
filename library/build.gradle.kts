0828f0e642aa9322b9fc762f6eb6b7f522ee1131
RafaGonP <rafael.gonzalez.peces@gmail.com>
2024-08-09 12:38:06 +0200

    RGP - more changes to build.gradle.kts to add compileSdkVersion

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -10,27 +10,6 @@ plugins {
 
 android {
     compileSdkVersion(Versions.COMPILE_SDK_VERSION)
-
-    defaultConfig {
-        applicationId = "com.RafaGonP.action.cable.client.sample"
-        minSdkVersion(Versions.MIN_SDK_VERSION)
-        targetSdkVersion(Versions.TARGET_SDK_VERSION)
-        versionCode = Versions.VERSION_CODE
-        versionName = Versions.VERSION_NAME
-        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            isShrinkResources = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    sourceSets["main"].withConvention(KotlinSourceSet::class) {
-        kotlin.srcDir("src/main/kotlin")
-    }
 }
 
 group = "com.github.RafaGonP"
