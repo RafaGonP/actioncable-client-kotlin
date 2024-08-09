f37ded4580b3416e1d1162864c6ff6632277d286
RafaGonP <rafael.gonzalez.peces@gmail.com>
2024-08-09 12:51:48 +0200

    RGP - more changes to build.gradle.kts back and forth

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,21 +1,53 @@
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import Versions.VERSION_NAME
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-android-extensions")
-    `maven-publish`
-}
-
-android {
-    compileSdkVersion(Versions.COMPILE_SDK_VERSION)
+    id("kotlin")
+    id("maven-publish")
 }
 
 group = "com.github.RafaGonP"
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.test {
+    useJUnitPlatform {
+        excludeTags("slow")
+        includeEngines("junit-jupiter")
+        failFast = true
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "$group"
+            artifactId = "actioncable-client-kotlin"
+            version = VERSION_NAME
+            from(components["java"])
+        }
+    }
+}
+
+tasks {
+    val sourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    val javadocJar by creating(Jar::class) {
+        dependsOn.add(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }
+
+    artifacts {
+        archives(sourcesJar)
+        archives(javadocJar)
+        archives(jar)
+    }
 }
 
 dependencies {
@@ -25,26 +57,6 @@ dependencies {
     implementation(Libs.GSON)
     testImplementation(Libs.JUNIT)
     testImplementation(Libs.MOCKWEBSERVER)
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            register<MavenPublication>("release") {
-                groupId = "com.rafagonp"
-                artifactId = "actioncable-client"
-                version = "1.1"
-
-                afterEvaluate {
-                    from(components["release"])
-                }
-            }
-        }
-        repositories {
-            maven {
-                name = "rafagonp_myrepo"
-                url = uri("${project.buildDir}/repo")
-            }
-        }
-    }
+    testImplementation(Libs.MOCKITO_KOTLIN)
+    testImplementation(Libs.MOCKITO_INLINE)
 }
