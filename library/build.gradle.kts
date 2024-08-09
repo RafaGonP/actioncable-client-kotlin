b33823145801161fcfeba5e0906fa4b7f0156ceb
RafaGonP <rafael.gonzalez.peces@gmail.com>
2024-08-09 12:10:12 +0200

    RGP - fixing after merge

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -3,11 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.gradle.api.publish.PublishingExtension
 
 plugins {
-    maven
     id("com.android.library")
     id("kotlin-android")
     id("kotlin-android-extensions")
-    id("kotlin")
     id("maven-publish")
 }
 
@@ -15,44 +13,6 @@ group = "com.github.RafaGonP"
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.test {
-    useJUnitPlatform {
-        excludeTags("slow")
-        includeEngines("junit-jupiter")
-        failFast = true
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "$group"
-            artifactId = "actioncable-client-kotlin"
-            version = VERSION_NAME
-            from(components["java"])
-        }
-    }
-}
-
-tasks {
-    val sourcesJar by creating(Jar::class) {
-        archiveClassifier.set("sources")
-        from(sourceSets.main.get().allSource)
-    }
-
-    val javadocJar by creating(Jar::class) {
-        dependsOn.add(javadoc)
-        archiveClassifier.set("javadoc")
-        from(javadoc)
-    }
-
-    artifacts {
-        archives(sourcesJar)
-        archives(javadocJar)
-        archives(jar)
-    }
 }
 
 dependencies {
@@ -72,7 +32,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.rafagonp"
                 artifactId = "actioncable-client"
-                version = "1.0"
+                version = "1.1"
 
                 afterEvaluate {
                     from(components["release"])
